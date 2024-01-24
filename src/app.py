e482b74c5f16a556c381075e56aa7141e4bfce66
MaximeDro <maximedroillard@gmail.com>
2024-01-24 16:30:24 +0100

    Correction pages

--- src/app.py
+++ src/app.py
@@ -1,13 +1,17 @@
 import dash
 from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__, use_pages=True)
+external_stylesheets = [dbc.themes.CERULEAN]
+app = Dash(__name__, use_pages=True, external_stylesheets=external_stylesheets)
+
+# app = Dash(__name__, use_pages=True)
 
 app.layout = html.Div([
-    html.H1('Multi-page app with Dash Pages'),
+    html.H1('Multi-page app with Dash Pages',style = 'center'),
     html.Div([
         html.Div(
-            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+            dcc.Link(f"{page['name']}", href=page["relative_path"])
         ) for page in dash.page_registry.values()
     ]),
     dash.page_container
