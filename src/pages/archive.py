12a08f756ec78deb8c7abe56cb44bf4793772236
MaximeDro <maximedroillard@gmail.com>
2024-01-24 16:58:15 +0100

    Blague

--- src/pages/archive.py
+++ src/pages/archive.py
@@ -6,4 +6,7 @@ dash.register_page(__name__)
 layout = html.Div([
     html.H1('This is our Archive page'),
     html.Div('This is our Archive page content.'),
+    html.H1("Bonjour je m'appelle Dalida "),
+    html.Br(),
+    html.H1('Dalida SH'),
 ])
